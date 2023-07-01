a3f6d1c58bec4fb1196f583621628bd163152ff5
Nishanth Chandra <thenameisnishanth@gmail.com>
2023-07-01 15:31:48 +0530

    Modified factorial.js

--- Factorial/factorial.js
+++ Factorial/factorial.js
@@ -3,29 +3,13 @@ const button = document.querySelector("button");
 const input = document.querySelector("input");
 const content = document.querySelector(".content");
 
-button.addEventListener("click", () => {
-    let number = parseInt(document.querySelector(".type").value);
-    if (number < 0) {
-        h2.innerText = `Factorial doesn't exist for ${number}`;
-    } else if (number === 0) {
-        h2.innerText = `Factorial for ${number} is 1`
-    } else {
-        let fact = number;
-        for (let i = fact - 1; i > 0; i--) {
-            fact *= i;
-        }
-        h2.innerText = `Factorial is ${fact}`;
-    }
-    content.appendChild(h2);
-})
-
-input.addEventListener("change", () => {
-    let number = parseInt(document.querySelector(".type").value);
+function buttonEvent(){
     button.addEventListener("click", () => {
+        let number = parseInt(document.querySelector(".type").value);
         if (number < 0) {
             h2.innerText = `Factorial doesn't exist for ${number}`;
         } else if (number === 0) {
-            h2.innerText = `Factorial of ${number} is 1`
+            h2.innerText = `Factorial for ${number} is 1`
         } else {
             let fact = number;
             for (let i = fact - 1; i > 0; i--) {
@@ -35,6 +19,10 @@ input.addEventListener("change", () => {
         }
         content.appendChild(h2);
     })
+}
+
+input.addEventListener("change", () => {
+ buttonEvent();
 })
 
 
